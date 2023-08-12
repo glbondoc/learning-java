31a584dd59f63b8fc2008ad06663c5db4e0a878d
glbondoc <genreininja@gmail.com>
2023-08-12 06:50:51 -0700

    add files

---
+++ Learning-java/src/main/java/learning/java/LearningJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package learning.java;
+
+/**
+ *
+ * @author genrei
+ */
+public class LearningJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
