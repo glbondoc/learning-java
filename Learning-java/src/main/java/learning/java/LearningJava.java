fca43be90d4dddf7d43d27cb8d4dcb4575ef3d22
glbondoc <genreininja@gmail.com>
2023-08-12 07:07:19 -0700

    write calculator

--- Learning-java/src/main/java/learning/java/LearningJava.java
+++ Learning-java/src/main/java/learning/java/LearningJava.java
@@ -8,9 +8,30 @@ package learning.java;
  *
  * @author genrei
  */
+import java.util.Scanner;
 public class LearningJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan= new Scanner (System.in);
+        
+        System.out.println("Enter first number: ");
+        int firstnum= scan.nextInt ();
+        System.out.println("Enter symbol: ");
+        char symbol= scan.next().charAt (0);
+        System.out.println("Enter second number: ");
+        int secondnum= scan.nextInt ();
+        
+        if (symbol == '+'){
+            System.out.println(firstnum+secondnum);
+        }
+        if (symbol == '-') {
+            System.out.println(firstnum-secondnum);
+        }
+        if (symbol == '*'){
+            System.out.println(firstnum*secondnum);
+        }
+        if (symbol == '/') {
+            System.out.println(firstnum/secondnum);
+        }
     }
 }
